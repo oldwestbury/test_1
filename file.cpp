3ca3fc34a59d9a7afda49b3c6adf886cd7b427be
oldwestbury <bestcodeguru@gmail.com>
2026-03-02 17:49:03 -0500

    Add binary file writing example in C++

---
+++ file.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <fstream>
+
+struct Data {
+    int id;
+    char name[20];
+    double score;
+};
+
+int main() {
+    std::ofstream outFile("data.bin", std::ios::binary);
+    if (!outFile) {
+        std::cerr << "Error opening file!" << std::endl;
+        return 1;
+    }
+
+    Data record = {1, "Alice", 95.5};
+    outFile.write(reinterpret_cast<char*>(&record), sizeof(record));
+
+    outFile.close();
+    return 0;
+}
